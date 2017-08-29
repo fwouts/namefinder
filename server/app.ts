80ad26afef741b167be702069f5a15d5bda737ab
Francois Wouts <f@codonut.com>
2017-08-29 12:14:42 +1000

    Add whois check endpoint

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,24 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
 
+import { exec } from "child_process";
+
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   respondJson<string>(res, "Hello, World!");
+});
+
+app.get("/domain/whois/:domainName", (req, res) => {
+  let domainName = req.params["domainName"];
+  if (!/^[a-z0-9]+\.[a-z]+$/.exec(domainName)) {
+    respondJson<string>(res, "Invalid domain name: " + domainName);
+    return;
+  }
+  exec(`whois ${domainName}`, (error, stdout, stderr) => {
+    respondJson<string>(res, stdout);
+  });
 });
 
 app.listen(3002, () => {
