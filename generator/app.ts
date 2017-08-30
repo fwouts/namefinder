9a3fdf5363a06dbf5b89b831464ee2b0a9304713
Francois Wouts <f@codonut.com>
2017-08-31 08:32:49 +1000

    Add Facebook and Twitter availability checks

--- generator/app.ts
+++ generator/app.ts
@@ -2,22 +2,24 @@ import * as chalk from "chalk";
 import * as randomWords from "random-words";
 
 import { exec } from "child_process";
+import fetch from "node-fetch";
 
 checkInfinitely();
 
 function checkInfinitely() {
-  checkRandomDomain().then(checkInfinitely);
+  checkRandomName().then(checkInfinitely);
 }
 
-function checkRandomDomain(): Promise<any> {
-  let domainName =
-    randomWords({
-      exactly: 2
-    }).join("") + ".io";
-  return isDomainAvailable(domainName)
-    .then(available => {
-      if (available) {
-        console.log(chalk.green(domainName));
+function checkRandomName(): Promise<any> {
+  let name = randomWords(2).join("");
+  return Promise.all([
+    isDomainAvailable(name, "io"),
+    isFacebookPageAvailable(name),
+    isTwitterPageAvailable(name)
+  ])
+    .then(availabilityList => {
+      if (availabilityList[0] && availabilityList[1] && availabilityList[2]) {
+        console.log(chalk.green(name + ".io"));
       }
     })
     .catch(e => {
@@ -25,13 +27,25 @@ function checkRandomDomain(): Promise<any> {
     });
 }
 
-function isDomainAvailable(domainName: string): Promise<boolean> {
+function isDomainAvailable(name: string, extension: string): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
-    exec(`whois ${domainName}`, (error, stdout, stderr) => {
+    exec(`whois ${name}.${extension}`, (error, stdout, stderr) => {
       if (error) {
         reject(error);
       }
       resolve(stdout.startsWith("NOT FOUND\n"));
     });
+  });
+}
+
+function isFacebookPageAvailable(name: string): Promise<boolean> {
+  return fetch(`https://www.facebook.com/${name}`).then(response => {
+    return response.status == 404;
+  });
+}
+
+function isTwitterPageAvailable(name: string): Promise<boolean> {
+  return fetch(`https://www.twitter.com/${name}`).then(response => {
+    return response.status == 404;
   });
 }
